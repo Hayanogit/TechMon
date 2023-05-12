3b0675de14a369e8ece2b5030b62f5b6fde6b112
HayatoOi <hyt7778to@gmail.com>
2023-05-12 19:47:42 +0900

    Initial Commit

---
+++ TechMon/BattleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BattleViewController.swift
+//  TechMon
+//
+//  Created by HAYATO OI on 2023/05/12.
+//
+
+import UIKit
+
+class BattleViewController: UIViewController {
+    
+    @IBOutlet var player
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
